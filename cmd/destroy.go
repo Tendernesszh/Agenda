0aca9a83c880fee3e0ff91ba95db41075bfcc3c6
CoolHorse <18142823152@163.com>
2017-10-29 18:11:16 +0800

    nothing is written here

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -15,7 +15,7 @@
 package cmd
 
 import (
-	"github.com/HinanawiTenshi/Agenda/entity"
+	"github.com/JasonZang1005/Agenda/entity"
 	"github.com/spf13/cobra"
 )
 
@@ -23,16 +23,10 @@ import (
 var destroyCmd = &cobra.Command{
 	Use:   "destroy",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `delete current user`,
 	Run: func(cmd *cobra.Command, args []string) {
 		curUser, _ := getCurUser()
 		entity.DeleteOneUser(curUser)
-
 	},
 }
 
--- cmd/quitmeeting.go
+++ cmd/quitmeeting.go
@@ -17,7 +17,7 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/HinanawiTenshi/Agenda/entity"
+	"github.com/JasonZang1005/Agenda/entity"
 	"github.com/spf13/cobra"
 )
 
@@ -27,12 +27,7 @@ var title string
 var quitmeetingCmd = &cobra.Command{
 	Use:   "quitmeeting",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `quit the meeting current user particapated`,
 	Run: func(cmd *cobra.Command, args []string) {
 		AllMeeting := entity.GetMeetings()
 		for _, meeting := range AllMeeting {
