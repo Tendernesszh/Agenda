4a2d1ad6b740a2b66d1b343eb75b8b541d5cf2f1
CoolHorse <18142823152@163.com>
2017-10-31 14:29:00 +0800

    updated

--- cmd/users.go
+++ cmd/users.go
@@ -39,12 +39,7 @@ func init() {
 var usersCmd = &cobra.Command{
 	Use:   "users",
 	Short: "A brief description of your command.....",
-	Long: `A longer description that spans multiple lines and likely contains examples
-				and usage of using your command. For example:
-
-				Cobra is a CLI library for Go thatasdwasdwad empowers applications.
-				This application is a tool to generate the needed files
-				to quickly create a Cobra application.`,
+	Long:  `show all the users`,
 	Run: func(cmd *cobra.Command, args []string) {
 		userList := entity.GetUsers()
 		for _, user := range userList {
